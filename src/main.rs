6d5542b72d990e753b0400e9a1627e73486fbc6f
Marshall Quander <marshall@quander.me>
2017-12-24 19:38:46 -0500

    Clarify assemble_b64_table

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,13 @@ fn main() {
 
 fn hex_to_b64<T, U>(reader: &mut T, writer: &mut U) -> Result<(), Box<Error>> where T: Read, U: Write {
     let mut buffer = [0; 6];
-    let b64_table = assemble_b64_table();
+    let alphabet = b64_alphabet();
     loop {
         let l = reader.read(&mut buffer)?;
         match buffer[..l].iter().rposition(|&b| (b as char).is_digit(16)) {
             None => { break; } // we read nothing but terminators
             Some(pos) => {     // pos is the index of the last hex digit
-                writer.write(&sextet_to_b64(&buffer[..pos+1], &b64_table))?;
+                writer.write(&sextet_to_b64(&buffer[..pos+1], &alphabet))?;
                 if pos+1 < l {
                     break;
                 }
@@ -27,23 +27,11 @@ fn hex_to_b64<T, U>(reader: &mut T, writer: &mut U) -> Result<(), Box<Error>> wh
     Ok(())
 }
 
-fn assemble_b64_table() -> [u8; 64] {
-    let mut table = [0; 64];
-    for i in 0..26 {
-        table[i] = i as u8 + 'A' as u8;
-    }
-    for i in 0..26 {
-        table[i + 26] = i as u8 + 'a' as u8
-    }
-    for i in 0..10 {
-        table[i + 52] = i as u8 + '0' as u8;
-    }
-    table[62] = '+' as u8;
-    table[63] = '/' as u8;
-    table
+fn b64_alphabet() -> &'static [u8] {
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".as_bytes()
 }
 
-fn sextet_to_b64(sextet: &[u8], b64_table: &[u8]) -> [u8; 4] {
+fn sextet_to_b64(sextet: &[u8], alphabet: &[u8]) -> [u8; 4] {
     let mut x: u32 = 0;
     let mut out_buffer = [0; 4];
     for i in 0..6 {
@@ -57,7 +45,7 @@ fn sextet_to_b64(sextet: &[u8], b64_table: &[u8]) -> [u8; 4] {
             let shift = (3 - i) * 6;
             let shifted = x >> shift;
             let char_val = shifted & 63;
-            b64_table[char_val as usize]
+            alphabet[char_val as usize]
         } else {
             b'='
         }
